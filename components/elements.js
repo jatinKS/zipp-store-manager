90f3179b123dab6c904e7d5a9c1626df9b27fd70
Jatin Gaurav Soni <jatin.soni007@gmail.com>
2021-10-28 09:23:38 +0530

    login screen done

--- components/elements.js
+++ components/elements.js
@@ -1,14 +1,68 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, TextInput, StyleSheet, TouchableOpacity, Text } from 'react-native';
 import { useTheme } from 'react-native-paper';
-
+import { FontAwesome } from '@expo/vector-icons';
 const Card = (props) => {
     const { colors } = useTheme();
     return (
-        <View style={{ backgroundColor: colors.surface }}>
+        <View style={{ ...props.style,backgroundColor: colors.surface }}>
             {props.children}
         </View>
     );
 }
  
-export { Card };
+const InputEl = (props) => {
+    const { label, icon, value, onChangeText } = props;
+    const { colors } = useTheme();
+    const elColor = colors.primary;
+    return (
+        <View style={{...styles.inputEl, borderBottomColor: elColor}}>
+            
+            <FontAwesome name={icon} size={24} color={elColor} />
+            <TextInput
+                placeholder={label}
+                style={styles.inputElTi}
+                value={value}
+                onChangeText={onChangeText}
+            />
+        </View>
+    )
+}
+
+const ButtonEl = (props) => {
+    const { label, onPress } = props;
+    const { colors } = useTheme();
+    const elColor = colors.primary;
+    return (
+        <TouchableOpacity style={{...styles.buttonEl,backgroundColor: elColor}}>
+            <Text style={styles.buttonElText}>{label}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    elMarginVer: 5,
+    elMarginHor: 10,
+    inputEl: {
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        padding: 2,
+        marginHorizontal: 5,
+        marginVertical: 10
+    },
+    inputElTi: {
+        paddingLeft: 5,
+        fontSize: 16,
+        flexGrow: 1
+    },
+    buttonElText: {
+        color: '#fff',
+        textAlign: 'center',
+        paddingVertical: 7
+    },
+    buttonEl: {
+        borderRadius: 5,
+        marginHorizontal: 5
+    }
+});
+export { Card, InputEl, ButtonEl };
--- screens/Login.js
+++ screens/Login.js
@@ -1,29 +1,37 @@
-import React from 'react';
-import { StyleSheet, View, Button, Image, Text } from 'react-native';
+import React,{ useState } from 'react';
+import { StyleSheet, View, Button, Image, Text, TextInput, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { useTheme } from 'react-native-paper';
-import { Card } from '../components/elements';
+import { Card, InputEl, ButtonEl } from '../components/elements';
+
 
 const Login = () => {
 
     const { colors } = useTheme();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: colors.background }}>
-            <Card>
-                <View style={{backgroundColor: 'pink', width: '80%'}}>
-
-                    <Image 
-                        source={require('../assets/logo.png')} 
-                        style={{
-                            width: 200,
-                            resizeMode: 'contain' 
-                        }}/>     
-                </View>
-                <Text>Hello</Text>
-                <Text style={{textAlign: 'center'}}>Hello</Text>
-            </Card>
-        </View>
+        <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()}>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: colors.background }}>
+                <Card style={{ padding : 20 }}>
+                    <View style={{width: '80%'}}>
+                        <Image 
+                            source={require('../assets/logo.png')} 
+                            style={{
+                                width: 300,
+                                height: 75,
+                                resizeMode: 'contain' 
+                            }}
+                        />     
+                    </View>
+                    
+                    <InputEl label='Username / Email' icon='user' value={email} onChangeText={setEmail}/>
+                    <InputEl label='Password' icon='lock' value={password} onChangeText={setPassword}/>
+                    <ButtonEl label='Login' />
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
     );
 }
- 
+
 export default Login;
